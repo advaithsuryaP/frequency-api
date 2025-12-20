8b1c4b211c5ac2826b9184efc04ca5d28926c82e
advaithsuryaP <padvaithsurya@gmail.com>
2025-12-20 15:36:21 -0500

    Add Users module with controller, service, DTOs, and tests; update app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AgentsModule } from './agents/agents.module';
 import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-    imports: [AgentsModule, PostsModule],
+    imports: [AgentsModule, PostsModule, UsersModule],
     controllers: [AppController],
     providers: [AppService]
 })
