23fd65582c49f74ace42bc8f325eebcfd1080de5
advaithsuryaP <padvaithsurya@gmail.com>
2026-01-08 23:48:12 -0500

    Enhance PaginationDto by adding IsPositive validation to skip and limit fields, ensuring only positive values are accepted for pagination parameters.

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,12 +1,14 @@
-import { IsNotEmpty, IsNumber, Min } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
     @IsNumber()
+    @IsPositive()
     @IsNotEmpty()
     @Min(0)
     skip: number;
 
     @IsNumber()
+    @IsPositive()
     @IsNotEmpty()
     @Min(1)
     limit: number;
