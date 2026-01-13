8a8c00e66dd04190f926ff592bef2b9d0241e0a0
advaithsuryaP <padvaithsurya@gmail.com>
2026-01-12 23:58:14 -0500

    Comment out JwtStrategy implementation in jwt.strategy.ts to disable JWT authentication temporarily while maintaining code structure for future use.

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -2,12 +2,12 @@ import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
-export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(configService: ConfigService) {
-        super({
-            ignoreExpiration: false,
-            secretOrKey: configService.get<string>('JWT_SECRET') ?? '',
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
-        });
-    }
-}
+// export class JwtStrategy extends PassportStrategy(Strategy) {
+//     // constructor(configService: ConfigService) {
+//     //     super({
+//     //         ignoreExpiration: false,
+//     //         secretOrKey: configService.get<string>('JWT_SECRET') ?? '',
+//     //         jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+//     //     });
+//     // }
+// }
