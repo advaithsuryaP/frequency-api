534bc3aad0d9ac3bce59d9d1055d46ccdba36dd4
advaithsuryaP <padvaithsurya@gmail.com>
2026-01-16 23:41:42 -0500

    Update RoleAuthGuard to enforce type safety by specifying the user type as PublicUser, ensuring consistent role validation in the authentication process.

--- src/modules/auth/guards/role-auth/role-auth.guard.ts
+++ src/modules/auth/guards/role-auth/role-auth.guard.ts
@@ -2,6 +2,7 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { RoleEnum } from '../../enum/role.enum';
 import { ROLES_KEY } from '../../decorators/roles.decorators';
+import { PublicUser } from 'src/modules/users/interfaces/public-user.interface';
 
 @Injectable()
 export class RoleAuthGuard implements CanActivate {
@@ -12,7 +13,7 @@ export class RoleAuthGuard implements CanActivate {
             context.getClass(),
         ]);
 
-        const user = context.switchToHttp().getRequest().user;
+        const user: PublicUser = context.switchToHttp().getRequest().user;
         const hasRequiredRoles = requiredRoles.some(role => user.role === role);
         return hasRequiredRoles;
     }
